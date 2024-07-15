97bd8ba133405c6b0f851ae053086bab256bce0f
Victor Parga <victorparga765@gmail.com>
2024-07-15 19:30:08 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <ChatModal />
+        {/* <ChatModal /> */}
         <Hero />
         <Benefits />
         <Collaboration />
